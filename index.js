b2282891e3930899f94df5298af80f3cb811ae64
Benjamin <b.oechsle@web.de>
2022-05-30 13:54:13 +0200

    basic finished

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const ShortUrl = require("./shortUrl");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded());
+
+app.listen(port, () => { console.log("App lisening on port " + port); });
+ShortUrl.initRedis();
+
+app.get("/",
+        (req, res) => { res.sendFile("/index.html", {root : __dirname}); });
+
+app.post(
+    "/shortUrls",
+    async (req, res) => { res.send(await ShortUrl.create(req.body.longUrl)); });
+
+app.post("/longUrls", async (req, res) => {
+  let longUrl = await ShortUrl.get(req.body.shortUrl);
+  res.redirect(longUrl);
+});
---
+++ shortUrl.js
@@ -0,0 +1,33 @@
+const {nanoid} = require("nanoid");
+const redis = require("redis");
+const client = redis.createClient();
+
+async function create(longUrl) {
+  let shortUrl = "https://peter.de/" + nanoid(8);
+  if ((await client.get(longUrl)) !== null) {
+    // check if URL is already in db
+    return await client.get(longUrl); // if URL is in db return shorturl
+  } else {
+    // if URL is not in db
+    if ((await client.get(shortUrl)) == null) {
+      // check if hash collision
+      client.multi().set(shortUrl, longUrl).set(longUrl, shortUrl).exec();
+      return client.get(longUrl);
+    } else {
+      // if collision call again
+      create(longUrl);
+    }
+  }
+}
+
+async function get(shortUrl) { return await client.get(shortUrl); }
+
+async function initRedis() {
+  client.on("error", (err) => console.log("Redis Client Error ", err));
+  await client.connect();
+}
+module.exports = {
+  create : create,
+  get : get,
+  initRedis : initRedis,
+};
